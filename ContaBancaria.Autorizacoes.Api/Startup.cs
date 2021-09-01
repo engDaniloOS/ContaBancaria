fe1399937f30ddb1c93fb4961d4f1ae6dc677202
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2021-09-01 19:17:08 -0300

    Ajuste nas configurações e swagger do projeto de autorizacao
    Ajuste nas configurações do projeto e adição do swagger

--- ContaBancaria.Autorizacoes.Api/Startup.cs
+++ ContaBancaria.Autorizacoes.Api/Startup.cs
@@ -1,24 +1,22 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using ContaBancaria.Autorizacoes.Api.Infraestrutura.BancoDeDados;
+using ContaBancaria.Autorizacoes.Api.Infraestrutura.Repositorios;
+using ContaBancaria.Autorizacoes.Api.Negocio.Servicos;
+using ContaBancaria.Autorizacoes.Api.Servicos;
+using ContaBancaria.Autorizacoes.Api.Servicos.Infraestrutura;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ContaBancaria.Autorizacoes.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
@@ -26,6 +24,25 @@ namespace ContaBancaria.Autorizacoes.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            #region Banco de dados
+            services.AddDbContext<Contexto>(opcao => opcao.UseSqlServer(GetConnectionString()));
+            #endregion
+
+            #region Injeção de dependência
+            services.AddTransient<ISessaoService, SessaoService>();
+            services.AddTransient<ITransacaoService, TransacaoService>();
+            services.AddTransient<IDispositivoRepository, DispositivoRepository>();
+            services.AddTransient<ISessaoRepository, SessaoRepository>();
+            services.AddTransient<ITransacaoRepository, TransacaoRepository>();
+            #endregion
+
+            #region Swagger
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "Conta Bancária - Autorização", Version = "v1" });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,8 +58,15 @@ namespace ContaBancaria.Autorizacoes.Api
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(s => s.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"));
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+
+        private string GetConnectionString()
+            => Configuration.GetConnectionString("Default").Replace("?path?", Environment.CurrentDirectory);
     }
 }
